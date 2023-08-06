1f8b0a898a91cc06a0d0e263bd4b26ede886c90d
jlipscomb071 <jlipscomb071@gmail.com>
2023-08-05 21:25:44 -0700

    Configured routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import {Dashboard} from './components/Dashboard'
+import {Expenses} from './components/Expenses'
+import {Reports} from './components/Reports'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Expense Explorer</h1>
-      </header>
-      <main className="App-main">
-        <div className="App-bubbles">
-          <a href="/dashboard" className="App-bubble">
-            <span className="App-bubble-text">Budget Dashboard</span>
-          </a>
-          <a href="/expenses" className="App-bubble">
-            <span className="App-bubble-text">Expenses</span>
-          </a>
-          <a href="/reports" className="App-bubble">
-            <span className="App-bubble-text">Spending Reports</span>
-          </a>
-        </div>
-      </main>
-    </div>
+    <Routes>
+      <Route path = '/' element = {<Dashboard />} ></Route>
+      <Route path = 'Expenses' element = {<Expenses />} ></Route>
+      <Route path = 'Reports' element = {<Reports />} ></Route>
+    </Routes>
   );
 }
 
