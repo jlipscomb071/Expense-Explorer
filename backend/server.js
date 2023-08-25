41795243a4e6de2cc0a2f4ff552e9043c037ed50
jlipscomb071 <jlipscomb071@gmail.com>
2023-08-25 12:38:32 -0500

    Added express connection to server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
-const dotenv = require("dotenv")
-const path = require("path")
-const { fileURLToPath } = require('url');
-const envPath = path.resolve(__dirname, '..', '.env')
+import express from "express"
+import expressConnection from "./routeHandler.js"
+import dotenv  from "dotenv"
+import path from "path"
+import { fileURLToPath } from "url"
+import mongoose from "mongoose"
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const envPath = path.resolve(dirname, '..', '.env')
 
 dotenv.config({ path: envPath })
 
-const mongoose = require('mongoose')
 const username = process.env.VITE_USERNAME
 const password = process.env.VITE_PASSWORD
 const cluster =  process.env.VITE_CLUSTER
 const dbname = process.env.VITE_DB
-
 
 const url = `mongodb+srv://${username}:${password}@${cluster}.ubzdpot.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
@@ -19,12 +23,19 @@ const connectionParams={
     useUnifiedTopology: true
 }
 
+const app = express()
+app.use(express.json())
+
 mongoose.connect(url, connectionParams)
-
-
-const connection = mongoose.connection;
+const connection = mongoose.connection
 
 connection.on("error", console.error.bind(console, "connection error: "))
 connection.once("open", async function () {
     console.log("Connected successfully");
+})
+
+app.use(expressConnection)
+
+app.listen(3000, () => {
+    console.log(`Server is running at port 3000`)
 })
