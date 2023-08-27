95100191c1bcfd138be980d83181478ddff0ad84
jlipscomb071 <jlipscomb071@gmail.com>
2023-08-27 13:28:57 -0500

    PUT and DELETE tests with timeouts

--- backend/routeHandler.js
+++ backend/routeHandler.js
@@ -6,7 +6,6 @@ const expressConnection = express()
 
 console.log("Entered Routes")
 
-
 // POST:
 expressConnection.post(`/${collection}`, async (request, response) => {
     const entry = new schema(request.body)
@@ -20,6 +19,7 @@ expressConnection.post(`/${collection}`, async (request, response) => {
     }
 })
 
+// GET:
 expressConnection.get(`/${collection}`, async (request, response) => {
     const entry = await schema.find({})
 
@@ -32,6 +32,42 @@ expressConnection.get(`/${collection}`, async (request, response) => {
     }
 })
 
+// PUT:
+expressConnection.put(`/${collection}/:id`, async (request, response) => {
+    const entryId = request.params.id
+    const update = {
+        checking: request.body.checking,
+        savings: request.body.savings
+    }
+    try{
+        const result =  await schema.findByIdAndUpdate(
+            entryId,
+            update)
+        if(result !== null){ 
+        console.log("Sucessfully updated entry: ", result)
+        } else {
+            console.log("Error: Entry Id doesn't exist, cannot update entry")
+        }
+    } catch(error){
+        response.status(500).send(error)
+        console.log("Error creating entry: ", error)
+    }
+}) 
 
+// DELETE:
+expressConnection.delete(`/${collection}/:id`, async (request, response) => {
+    const entryId = request.params.id
+    try{
+        const result = await schema.findByIdAndDelete(entryId)
+        if(result !== null){
+        console.log("Successfully deleted entry: ", result)
+        } else{
+            console.log("Error: Entry Id doesn't exist, cannot delete entry")
+        }
+    } catch (error){
+        response.status(500).send(error)
+        console.log("Error deleting entry: ", error)
+    }
+})
 
 export default expressConnection
--- backend/routeTester.js
+++ backend/routeTester.js
@@ -1,19 +1,21 @@
 import fetch from 'node-fetch'
 import { collection } from './schemas/defaultSchema.js'
 
-
-const body = {
-    checking: 100,
-    savings: 8500
-}
+// const body = {
+//    checking: 100,
+//    savings: 8500
+// }
+// const controller = new AbortController();
+// const signal = controller.signal;
 
 //POST TEST:
-// fetch(`http://localhost:3000/${collection}`, {
+// const fetchPromise = fetch(`http://localhost:3000/${collection}`, {
 //     method: "POST",
 //     body: JSON.stringify(body),
 //     headers: {
 //         "Content-type": "application/json; charset=UTF-8"
-//     }
+//     },
+//     signal
 // })
 // .then((response) => {
 //     if (!response.ok) {
@@ -33,23 +35,64 @@ const body = {
 //     console.error("POST request failed:", error)
 // })
 
-
 // GET TEST:
-fetch(`http://localhost:3000/${collection}`)
-.then((response) => {
-    if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`)
-    }
-    return response.json()
-})
-.then((json) => {
-    console.log("GET request succeeded:", json)
-})
-.catch((error) => {
-    console.error("GET request failed:", error)
-})
+// const fetchPromise = fetch(`http://localhost:3000/${collection}`)
+// .then((response) => {
+//     if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`)
+//     }
+//     return response.json()
+// })
+// .then((json) => {
+//     console.log("GET request succeeded:", json)
+// })
+// .catch((error) => {
+//     console.error("GET request failed:", error)
+// })
 
+// PUT TEST:
+// const bodyUpdate = {
+//     checking: 700,
+//     savings: 10000
+// };
 
+// const entryId = "64eb8655f8dc3d02e0480cf2";
+// const url = `http://localhost:3000/${collection}/${entryId}`;
 
+// const fetchPromise = fetch(url, {
+//     method: "PUT",
+//     body: JSON.stringify(bodyUpdate),
+//     headers: {
+//         "Content-type": "application/json; charset=UTF-8"
+//     },
+//     signal
+// });
 
+// DELETE TEST:
+// const entryId = "64eb8655f8dc3d02e0480cf3"
+// const fetchPromise = fetch(`http://localhost:3000/${collection}/${entryId}`, {
+//     method: "DELETE",
+//     headers: {
+//         "Content-type": "application/json; charset=UTF-8"
+//     },
+//     signal
+// })
 
+// ENSURE TIMEOUT:
+// const timeoutPromise = new Promise((resolve) => setTimeout(() => {
+//     controller.abort(); 
+//     resolve({ timeout: true });
+// }, 5000));
+
+// Promise.race([
+//     fetchPromise,
+//     timeoutPromise
+// ])
+//     .then((result) => {
+//         if (result.timeout) {
+//         } else {
+//             return result.json();
+//         }
+//     })
+//     .catch((error) => {
+//     });
