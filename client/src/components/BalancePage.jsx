9728a1050a85eac0316e21e9f5a90b50f8452635
jlipscomb071 <jlipscomb071@gmail.com>
2023-09-04 19:37:47 -0500

    layout  - update balance button feature on balance card  - update balance popup window

--- client/src/components/BalancePage.jsx
+++ client/src/components/BalancePage.jsx
@@ -3,6 +3,8 @@ import { collection } from "../../../backend/schemas/defaultSchema.js"
 
 const BalancePage = () => {
   const [checking, setChecking] = useState(null)
+  const [showUpdateForm, setShowUpdateForm] = useState(false)
+  const [depositAmount, setDepositAmount] = useState("")
 
   useEffect(() => {
     fetchCheckingBalance()
@@ -14,11 +16,24 @@ const BalancePage = () => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`)
       }
-      const json = await response.json();
+      const json = await response.json()
       setChecking(json[0]?.checking || null)
     } catch (error) {
       console.error("GET request failed:", error)
     }
+  }
+
+  const handleUpdateClick = () => {
+    setShowUpdateForm(true)
+  }
+
+  const handleDepositChange = (event) => {
+    setDepositAmount(event.target.value)
+  }
+
+  const handleConfirmClick = () => {
+    // Add your logic here to handle storing the deposit amount
+    setShowUpdateForm(false)
   }
 
   return (
@@ -27,7 +42,23 @@ const BalancePage = () => {
       <div style={styles.balanceTag}>
         <h2>Checking Balance:</h2>
         <p style={styles.balanceAmount}>${checking}</p>
+        <button onClick={handleUpdateClick}>Update</button>
       </div>
+      {showUpdateForm && (
+        <div style={styles.modalOverlay}>
+          <div style={styles.updateForm}>
+          <button style={styles.closeButton} onClick={() => setShowUpdateForm(false)}>x</button>
+            <h2>Enter New Deposit Amount:</h2>
+            <input
+              type="number"
+              placeholder="Enter amount"
+              value={depositAmount}
+              onChange={handleDepositChange}
+            />
+            <button style={styles.confirmButton} onClick={handleConfirmClick}>Confirm</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
@@ -35,20 +66,48 @@ const BalancePage = () => {
 const styles = {
   balanceTag: {
     backgroundColor: "#2A5D8F",
-    color: "white", 
+    color: "white",
     padding: "20px",
     borderRadius: "10px",
     boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
     textAlign: "center",
-    marginBottom: "20px"
+    marginBottom: "20px",
+    position: "relative",
   },
   balanceAmount: {
     fontSize: "24px",
     fontWeight: "bold",
-    marginTop: "10px"
+    marginTop: "10px",
+  },
+  modalOverlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    background: "rgba(0, 0, 0, 0.5)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  updateForm: {
+    backgroundColor: "black",
+    padding: "20px",
+    borderRadius: "5px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+    textAlign: "center",
+  },
+  confirmButton: {
+    marginTop: "10px",
+    marginLeft: "10px", // Add margin to move the button to the right
+  },
+  closeButton: {
+    backgroundColor: "red",
+    position: "absolute",
+    top: "10px",
+    right: "10px",
+    cursor: "pointer",
   }
 }
-
-
 
 export default BalancePage
